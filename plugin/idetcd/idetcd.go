06e2da2336715979683430f19784ae766978dea8
Jiacheng Xu <xjcmaxwell@163.com>
2018-05-25 19:39:59 +0200

    Add get and set to communicate with etcd.

--- plugin/idetcd/idetcd.go
+++ plugin/idetcd/idetcd.go
@@ -4,21 +4,56 @@ package idetcd
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/coredns/coredns/plugin"
+	etcdc "github.com/coreos/etcd/client"
 	"github.com/miekg/dns"
 )
 
 //Idetcd is a plugin which can configure the cluster without collison.
 type Idetcd struct {
-	Next plugin.Handler
+	Next      plugin.Handler
+	Ctx       context.Context
+	Client    etcdc.KeysAPI
+	endpoints []string
 }
 
 //ServeDNS implements the plugin.Handler interface
-func (idetcd Idetcd) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	fmt.Println("hello world!")
+func (idetcd *Idetcd) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	//state := request.Request{W: w, Req: r}
+
+	resp, _ := idetcd.get("Test")
+	fmt.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
+	//a := new(dns.Msg)
+	//a.SetReply(r)
+	//a.Authoritative = true
+	//w.WriteMsg(a)
 	return plugin.NextOrFailure(idetcd.Name(), idetcd.Next, ctx, w, r)
 }
 
+//set is a wrapper for client.Set
+func (idetcd *Idetcd) set(key string, value string) (*etcdc.Response, error) {
+
+	ctx, cancel := context.WithTimeout(idetcd.Ctx, 5*time.Second)
+	defer cancel()
+	r, err := idetcd.Client.Set(ctx, key, value, nil)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+// get is a wrapper for client.Get
+func (idetcd *Idetcd) get(key string) (*etcdc.Response, error) {
+	ctx, cancel := context.WithTimeout(idetcd.Ctx, 5*time.Second)
+	defer cancel()
+	r, err := idetcd.Client.Get(ctx, key, nil)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
 //Name implements the Handler interface.
-func (idetcd Idetcd) Name() string { return "idetcd" }
+func (idetcd *Idetcd) Name() string { return "idetcd" }
--- plugin/idetcd/setup.go
+++ plugin/idetcd/setup.go
@@ -1,8 +1,11 @@
 package idetcd
 
 import (
+	"context"
+
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
+	etcdc "github.com/coreos/etcd/client"
 
 	"github.com/mholt/caddy"
 )
@@ -16,12 +19,48 @@ func init() {
 
 func setup(c *caddy.Controller) error {
 	c.Next() //idetcd
+	idetc, err := idetcdParse(c)
+	if err != nil {
+		return plugin.Error("idetcd", err)
+	}
 	if c.NextArg() {
 		return plugin.Error("idetcd", c.ArgErr())
 	}
-	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-		return Idetcd{Next: next}
-	})
 
+	idetc.set("Test", "127.0.0.1")
+	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		idetc.Next = next
+		return idetc
+	})
 	return nil
 }
+
+func idetcdParse(c *caddy.Controller) (*Idetcd, error) {
+	idetc := Idetcd{
+		Ctx: context.Background(),
+	}
+	var (
+		endpoints = []string{defaultEndpoint}
+	)
+	client, err := newEtcdClient(endpoints)
+	if err != nil {
+		return &Idetcd{}, err
+	}
+	idetc.endpoints = endpoints
+	idetc.Client = client
+	return &idetc, nil
+
+}
+
+func newEtcdClient(endpoints []string) (etcdc.KeysAPI, error) {
+	etcdCfg := etcdc.Config{
+		Endpoints: endpoints,
+	}
+	cli, err := etcdc.New(etcdCfg)
+	if err != nil {
+		return nil, err
+	}
+	return etcdc.NewKeysAPI(cli), nil
+}
+
+const defaultEndpoint = "http://localhost:2379"
